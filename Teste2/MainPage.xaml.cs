67144cd5fb62f79eb061ba7cc3d022b12f7a18d9
Leko <lekogomes2013@gmail.com>
2024-10-26 22:40:45 -0300

    Adicionando a imagem de comparação ao clicar no botão

--- Teste2/MainPage.xaml.cs
+++ Teste2/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using OpenCvSharp;
 using OpenCvSharp.Features2D;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Size = OpenCvSharp.Size;
 
 namespace FingerprintComparisonApp
@@ -9,12 +11,12 @@ namespace FingerprintComparisonApp
     public partial class MainPage : ContentPage
     {
         private DatabaseService dbService;
-        private string imagePath1; // Variável global para armazenar o caminho da primeira imagem
+        private string imagePath1;
 
         public MainPage()
         {
             InitializeComponent();
-            dbService = new DatabaseService(); // Inicializa o serviço de banco de dados
+            dbService = new DatabaseService();
         }
 
         private async Task<string> PickImageAsync()
@@ -24,75 +26,48 @@ namespace FingerprintComparisonApp
                 var result = await FilePicker.PickAsync(new PickOptions
                 {
                     PickerTitle = "Escolha uma impressão digital",
-                    FileTypes = FilePickerFileType.Images // Define que só pode selecionar imagens
+                    FileTypes = FilePickerFileType.Images
                 });
 
                 if (result != null)
                 {
-                    return result.FullPath; // Retorna o caminho completo da imagem selecionada
+                    return result.FullPath;
                 }
                 return null;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                //await DisplayAlert("Erro", $"Falha ao selecionar a imagem: {ex.Message}", "OK");
                 return null;
             }
         }
 
         private async void OnSelectImage1Clicked(object sender, EventArgs e)
         {
-            imagePath1 = await PickImageAsync();  // Chama o método PickImageAsync para obter o caminho da imagem selecionada
+            imagePath1 = await PickImageAsync();
             if (!string.IsNullOrEmpty(imagePath1))
             {
-                // Converte o caminho da imagem para UTF-8
                 imagePath1 = Path.GetFullPath(imagePath1);
-
-                LabelImage1.Text = $"Imagem 1: {Path.GetFileName(imagePath1)}";  // Atualiza o rótulo (label) na interface
-                ButtonCompare.IsEnabled = true;  // Habilita o botão de comparação
+                LabelImage1.Text = $"Imagem 1: {Path.GetFileName(imagePath1)}";
+                ButtonCompare.IsEnabled = true;
             }
         }
 
-        private async void OnAddFingerprintClicked(object sender, EventArgs e)
+        private async Task<Mat> CreateMatchImage(Mat img1, Mat img2, List<DMatch> goodMatches, KeyPoint[] keypoints1, KeyPoint[] keypoints2)
         {
-            string imagePath = await PickImageAsync(); // Seleciona uma imagem
-            string nome = EntryNome.Text;
-            string cargo = CargoPicker.SelectedItem?.ToString();
-            if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(cargo))
-            {
-                await DisplayAlert("Erro", "Por favor, digite o nome e selecione o cargo.", "OK");
-                return;
-            }
-
-            imagePath = await PickImageAsync(); // Seleciona uma imagem
-            if (!string.IsNullOrEmpty(imagePath))
-            {
-                // Adiciona a impressão digital junto com o nome e o cargo ao banco de dados
-                dbService.AddFingerprint(imagePath, nome, cargo);
-                await DisplayAlert("Sucesso", "Impressão digital adicionada ao banco de dados.", "OK");
-            }
-            else
-            {
-                await DisplayAlert("Erro", "Por favor, selecione uma imagem.", "OK");
-            }
+            Mat matchImage = new Mat();
+            Cv2.DrawMatches(img1, keypoints1, img2, keypoints2, goodMatches, matchImage, flags: DrawMatchesFlags.NotDrawSinglePoints);
+            return matchImage;
         }
 
-        private async Task<bool> CompareFingerprints(string imagePath1, string imagePath2)
+        private async Task<bool> CompareFingerprintsAndShowMatches(string imagePath1, string imagePath2)
         {
             try
             {
-                // Converte os caminhos das imagens para UTF-8
-                imagePath1 = Path.GetFullPath(imagePath1);
-                imagePath2 = Path.GetFullPath(imagePath2);
-
-                // Carregar as duas imagens em escala de cinza
                 Mat img1 = Cv2.ImRead(imagePath1, ImreadModes.Grayscale);
                 Mat img2 = Cv2.ImRead(imagePath2, ImreadModes.Grayscale);
 
-                // Verificar se as imagens foram carregadas corretamente
                 if (img1.Empty() || img2.Empty())
                 {
-                    //await DisplayAlert("Erro", "Erro ao carregar as imagens. Verifique os caminhos dos arquivos.", "OK");
                     return false;
                 }
 
@@ -102,20 +77,16 @@ namespace FingerprintComparisonApp
                 Cv2.EqualizeHist(img1, img1);
                 Cv2.EqualizeHist(img2, img2);
 
-                // Inicializar o detector SIFT
+                // Inicializa o detector SIFT
                 var sift = SIFT.Create();
-
-                // Detectar pontos-chave e descritores
                 KeyPoint[] keypoints1, keypoints2;
                 Mat descriptors1 = new Mat(), descriptors2 = new Mat();
                 sift.DetectAndCompute(img1, null, out keypoints1, descriptors1);
                 sift.DetectAndCompute(img2, null, out keypoints2, descriptors2);
 
-                // Usar BFMatcher com o Ratio Test
                 var bf = new BFMatcher(NormTypes.L2, crossCheck: false);
                 var matches = bf.KnnMatch(descriptors1, descriptors2, k: 2);
 
-                // Aplicar o Ratio Test
                 List<DMatch> goodMatches = new List<DMatch>();
                 foreach (var match in matches)
                 {
@@ -125,45 +96,74 @@ namespace FingerprintComparisonApp
                     }
                 }
 
-                // Critério de compatibilidade
                 double matchPercentage = goodMatches.Count / (double)keypoints1.Length;
-                if (matchPercentage > 0.15) // Ajuste o valor conforme necessário
+                if (matchPercentage > 0.15) // Ajuste conforme necessário
                 {
-                    return true;
+                    Mat matchImage = await CreateMatchImage(img1, img2, goodMatches, keypoints1, keypoints2);
+                    string matchImagePath = Path.Combine(FileSystem.CacheDirectory, "matchImage.jpg");
+                    matchImage.SaveImage(matchImagePath);
+
+                    // Aqui você define a imagem de comparação como a fonte para FullScreenImage
+                    FullScreenImage.Source = ImageSource.FromFile(matchImagePath);
+                    FullScreenImage.IsVisible = true; // Torna a imagem visível
+                    MainContentLayout.IsVisible = false; // Oculta o layout principal
+
+                    return true; // Indica que houve uma correspondência
                 }
-                else
-                {
-                    return false;
-                }
+                return false; // Nenhuma correspondência encontrada
             }
             catch (Exception ex)
             {
-                //await DisplayAlert("Erro", $"Falha ao comparar as impressões digitais: {ex.Message}", "OK");
+                await DisplayAlert("Erro", $"Falha ao comparar as impressões digitais: {ex.Message}", "OK");
                 return false;
             }
         }
 
-
         private async void OnCompareFingerprintsClicked(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(imagePath1)) // Verifica se uma imagem foi selecionada
+            if (!string.IsNullOrEmpty(imagePath1))
             {
-                var fingerprints = dbService.GetAllFingerprints(); // Obtém todas as impressões digitais do banco
+                var fingerprints = dbService.GetAllFingerprints();
 
                 foreach (var fingerprint in fingerprints)
                 {
-                    bool isMatch = await CompareFingerprints(imagePath1, fingerprint.ImagePath); // Compara com as impressões armazenadas
+                    // Compara a imagem selecionada com a imagem do banco de dados
+                    bool isMatch = await CompareFingerprintsAndShowMatches(imagePath1, fingerprint.ImagePath);
                     if (isMatch)
                     {
                         string nome = fingerprint.Nome;
                         string cargo = fingerprint.Cargo;
+                        await Task.Delay(4000);
 
-                        await Navigation.PushAsync(new Teste(nome, cargo)); // Passa nome e cargo para a página Teste
-                        return;
+                        // Navega para a página Teste após a visualização da imagem de comparação
+                        await Navigation.PushAsync(new Teste(nome, cargo));
+                        return; // Sai do loop após encontrar a primeira correspondência
                     }
                 }
-
                 await DisplayAlert("Acesso Negado!", "Impressão digital não encontrada no banco de dados.", "OK");
+            }
+        }
+
+        private async void OnAddFingerprintClicked(object sender, EventArgs e)
+        {
+            string imagePath = await PickImageAsync(); // Seleciona uma imagem
+            string nome = EntryNome.Text; // Certifique-se de que EntryNome está definido no XAML
+            string cargo = CargoPicker.SelectedItem?.ToString(); // Certifique-se de que CargoPicker está definido no XAML
+            if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(cargo))
+            {
+                await DisplayAlert("Erro", "Por favor, digite o nome e selecione o cargo.", "OK");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                // Adiciona a impressão digital junto com o nome e o cargo ao banco de dados
+                dbService.AddFingerprint(imagePath, nome, cargo);
+                await DisplayAlert("Sucesso", "Impressão digital adicionada ao banco de dados.", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Por favor, selecione uma imagem.", "OK");
             }
         }
     }
